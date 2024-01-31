450a226eee8db231ee5970115383dd659c45fd75
Lagacy_code <miraglioenzo93@gmail.com>
2024-01-31 14:54:21 +0100

    test

--- build/resources/js/Pages/UndefinedPage.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-const UndefinedPage = () => {
-    return ((0, jsx_runtime_1.jsx)("div", { children: (0, jsx_runtime_1.jsx)("h1", { children: "Undefined Page" }) }));
-};
-exports.default = UndefinedPage;
-//# sourceMappingURL=UndefinedPage.js.map
